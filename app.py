0a9b725c10ea04190a5611a1368180318b55bdd7
angelsbcode <pedroangelsantos2003@gmail.com>
2026-05-29 18:23:20 -0600

    errores y panel lateral

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/index' \
-'')
-def index():
+#@app.route('/')
+#def index():
     # Aquí en el futuro puedes hacer las consultas a la base de datos 
     # y enviarlas a la plantilla HTML usando render_template
-    return render_template('/index.html')
+#    return render_template('/index.html')
 
-@app.route('/login')
+@app.route('/') #@app.route('/')
 def login():
     # Aquí en el futuro puedes hacer las consultas a la base de datos 
     # y enviarlas a la plantilla HTML usando render_template
@@ -27,10 +26,17 @@ def home():
     # y enviarlas a la plantilla HTML usando render_template
     return render_template('/home.html')
 
-@app.route('/')
+@app.route('/ajustes')
+def ajustes():
+    # Aquí en el futuro puedes hacer las consultas a la base de datos 
+    # y enviarlas a la plantilla HTML usando render_template
+    return render_template('/ajustes.html')
+
+@app.route('/perfil')
 def perfil():
-    # Aquí consultarás la tabla HOR_Usuario filtrando por el ID del usuario logueado
-    return render_template('perfil.html')
+    # Aquí en el futuro puedes hacer las consultas a la base de datos 
+    # y enviarlas a la plantilla HTML usando render_template
+    return render_template('/perfil.html')
 
 
 if __name__ == '__main__':
