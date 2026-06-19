8b495b485d26140f03d932cde889417f530eb71f
angelsbcode <pedroangelsantos2003@gmail.com>
2026-06-18 21:40:39 -0600

    css de perfil acutaliada y ajustes menores en al app.py para mas detalles

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # MÉTODOS FLASK
+
+# Home es la pagina despues de iniciar sesion
 @app.route('/')
 def index():
     # Aquí en el futuro puedes hacer las consultas a la base de datos 
@@ -23,11 +25,14 @@ def registro():
     # y enviarlas a la plantilla HTML usando render_template
     return render_template('/registro.html')
 
-@app.route('/home')
+
+# pagina de bienvenida al entrar a la pagina (esta fuera de los templates por alguna razon por lo que aun no funcion)
+# deberia ser la pagina principañ
+@app.route('/horworks')
 def home():
     # Aquí en el futuro puedes hacer las consultas a la base de datos 
     # y enviarlas a la plantilla HTML usando render_template
-    return render_template('/home.html')
+    return render_template('/horworks.html')
 
 @app.route('/ajustes')
 def ajustes():
